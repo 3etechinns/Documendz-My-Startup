91ae742228f52d774d87cce07d0bf972b112a880
root <root@documendz-System-Product-Name.(none)>
2014-11-27 12:08:21 +0530

    hide,jpeg,order by

---
+++ file_hide.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'functions.php';
+
+session_start();
+include 'connect.php';
+
+$id = mysql_real_escape_string($_POST['id']);
+$state = mysql_real_escape_string($_POST['hid_state']);
+
+mysql_query("UPDATE shared_files SET hidden_state=".$state." WHERE shared_id=".$id);
+
+
+
+mysql_close($dbhandle);
+
+?>
