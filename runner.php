d73d28893675cfce873ffd6b3957329288bcdcc9
root <root@documendz-System-Product-Name.(none)>
2015-03-03 18:14:46 +0530

    sample inputs

--- runner.php
+++ runner.php
@@ -11,10 +11,10 @@ for($i = 131; $i <=375; $i++){
     $r1 = getToken(20);
     $r2 = getToken(20);
     
-    // $s1 = getToken(15);
-    // $s2 = getToken(15);
-    // $s3 = getToken(15);
-    // $s4 = getToken(15);
+    $s1 = getToken(15);
+    $s2 = getToken(15);
+    $s3 = getToken(15);
+    $s4 = getToken(15);
 /// 2 sample wkgroups ///
 
 $n = mysqli_fetch_assoc(mysqli_query($dbhandle,"SELECT username FROM signup WHERE userid =".$i));
@@ -23,7 +23,13 @@ $n = mysqli_fetch_assoc(mysqli_query($dbhandle,"SELECT username FROM signup WHER
                         VALUES ('".$r1."','Documents (Sample)','This is where you can add a short description for your workgroup to let your collaborators know a few details about the files, deadlines, tasks that you need to complete. ',". $i.",'".$n['username']."',1),
                                ('".$r2."','Designs (Sample)','Example: Let us get the final draft for these designs by 18th Dec. Add your designs and revisions here.',". $i.",'".$n['username']."',1)");
 	
+$my_date = date("Y-m-d H:i:s");
 
+	mysqli_query($dbhandle,"INSERT INTO files VALUES('','".$s1."','Legal document.pdf','".$r1."',".$i.",'$my_date','pdf',11),
+	                                                ('','".$s2."','Research paper.pdf','".$r1."',".$i.",'$my_date','pdf',12),
+						                            ('','".$s3."','Creative design.jpg','".$r2."',".$i.",'$my_date','pdf',21),
+						                            ('','".$s4."','Floor plan.jpg','".$r2."',".$i.",'$my_date','pdf',22)");
+    
 
 }
 
