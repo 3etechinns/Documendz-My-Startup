87d0e02f6bac83b92f8ada0995fb83d6a3328f9a
root <root@documendz-System-Product-Name.(none)>
2015-07-11 10:25:07 +0530

    prog n all

--- a/app.js
+++ a/app.js
@@ -1435,7 +1435,7 @@ console.log($scope.fileName);
             }).progress(function(evt) {
                 $scope.showProgress = 1;
 	                      	
-console.log("in here");
+
 	                        $scope.progressValue = parseInt(100.0 * evt.loaded / evt.total);
 
 	                        try{
