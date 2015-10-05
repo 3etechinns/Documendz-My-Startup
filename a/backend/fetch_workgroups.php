b624d6ce197e4cfa446021e94275c0ba6949eeec
root <root@documendz-System-Product-Name.(none)>
2015-10-05 17:06:44 +0530

    change

--- a/backend/fetch_workgroups.php
+++ a/backend/fetch_workgroups.php
@@ -9,15 +9,15 @@ include 'connect.php';
 
 
 $result = mysqli_query($dbhandle, "SELECT wid, uniqueId, wname, wdesc, auth_id, auth_name, UNIX_TIMESTAMP(time)*1000 as time, sample  from workgroups where auth_id =".$_SESSION['userid']." OR uniqueId IN (SELECT wkuniqueId FROM collaborators where collabEmail = '".$_SESSION['email']."')");
-$emails = mysqli_query($dbhandle,"SELECT wkUniqueId,collabEmail from collaborators where wkUniqueId IN (SELECT uniqueId from workgroups where auth_id =".$_SESSION['userid']." OR uniqueID IN (SELECT wkuniqueId FROM collaborators where collabEmail = '".$_SESSION['email']."'))"); 
+//$emails = mysqli_query($dbhandle,"SELECT wkUniqueId,collabEmail from collaborators where wkUniqueId IN (SELECT uniqueId from workgroups where auth_id =".$_SESSION['userid']." OR uniqueID IN (SELECT wkuniqueId FROM collaborators where collabEmail = '".$_SESSION['email']."'))"); 
 //$files = mysqli_query($dbhandle,"SELECT workgroupid,filename from files where workgroupid IN (SELECT uniqueId from workgroups where auth_id =".$_SESSION['userid']." OR uniqueID IN (SELECT wkuniqueId FROM collaborators where collabEmail = '".$_SESSION['email']."'))");
 
 
-$data_emails = array();
+//$data_emails = array();
 
-while ($row_email = mysqli_fetch_assoc($emails)) {
-  $data_emails[$row_email['wkUniqueId']][] = $row_email['collabEmail'];
-}
+// while ($row_email = mysqli_fetch_assoc($emails)) {
+//   $data_emails[$row_email['wkUniqueId']][] = $row_email['collabEmail'];
+// }
 
 //print json_encode($data_emails);
 
@@ -29,7 +29,7 @@ while ($row = mysqli_fetch_assoc($result)) {
   
  
 }
-  $data['emails'][] = $data_emails;
+//  $data['emails'][] = $data_emails;
 
 print json_encode($data) ;
  ?>
